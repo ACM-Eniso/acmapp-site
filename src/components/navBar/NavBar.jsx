81ca19c0e50c60f03227a660241f26fe2ec6139a
Amer <ameur.bellil@eniso.u-sousse.tn>
2024-08-17 12:09:08 +0100

    small change

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import './NavBar.css';
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.webp'
 import menu_icon from '../../assets/menu-icon.png'
 import {useEffect, useState} from "react";
 import {Link} from 'react-scroll';
